0916954adac5972ec0bd61dc6c18be0d5cdf0919
luisbarajasr <luis.br02@hotmail.com>
2023-11-29 12:40:55 -0600

    lougut

--- app/src/main/java/com/example/nuevo_amanecer_app/screens/MenuScreen.kt
+++ app/src/main/java/com/example/nuevo_amanecer_app/screens/MenuScreen.kt
@@ -23,68 +23,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.nuevo_amanecer_app.R
 
 @Composable
 fun MenuScreen(navController: NavController) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            // Tablero column
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(20.dp)
-                        .height(450.dp) // Reduced height to 450dp from 500dp
-                        .width(450.dp) // Reduced width to 450dp from 500dp
-                        .clip(shape = RoundedCornerShape(20.dp))
-                        .background(color = Color(android.graphics.Color.parseColor("#FBECB2")))
-                        .clickable(onClick = {
-                            navController.navigate("HomeScreen")
-                        }),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(Icons.Default.Menu, contentDescription = "Menu", tint = Color.Black, modifier = Modifier.size(250.dp)) // Reduced size to 250dp
-                }
-                Text(text = "Tablero", fontSize = 40.sp) // Reduced font size to 40sp
-            }
 
-            // Juegos column
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(20.dp)
-                        .height(450.dp) // Reduced height to 450dp from 500dp
-                        .width(450.dp) // Reduced width to 450dp from 500dp
-                        .clip(shape = RoundedCornerShape(20.dp))
-                        .background(color = Color(android.graphics.Color.parseColor("#F1B4BB")))
-                        .clickable(onClick = {
-                            navController.navigate("JuegosMenuScreen")
-                        }),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(Icons.Default.Home, contentDescription = "Home", tint = Color.Black, modifier = Modifier.size(250.dp)) // Reduced size to 250dp
-                }
-                Text(text = "Juegos", fontSize = 40.sp) // Reduced font size to 40sp
-            }
-        }
+    // Logout button aligned to the bottom right
 
-        // Logout button aligned to the bottom right
+    // Logout button aligned to the bottom right
+    Box(){
         Button(
             onClick = {
                 navController.navigate("LoginPage") {
@@ -94,10 +45,63 @@ fun MenuScreen(navController: NavController) {
                 }
             },
             modifier = Modifier
-                .align(Alignment.BottomEnd)
+                .align(Alignment.TopEnd)
                 .padding(20.dp)
         ) {
-            Text("Logout", fontSize = 20.sp)
+            Icon(painter = painterResource(id = R.drawable.logout), contentDescription = "logout", modifier = Modifier.size(40.dp))
         }
     }
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Tablero column
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .height(450.dp) // Reduced height to 450dp from 500dp
+                    .width(450.dp) // Reduced width to 450dp from 500dp
+                    .clip(shape = RoundedCornerShape(20.dp))
+                    .background(color = Color(android.graphics.Color.parseColor("#FBECB2")))
+                    .clickable(onClick = {
+                        navController.navigate("HomeScreen")
+                    }),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(Icons.Default.Menu, contentDescription = "Menu", tint = Color.Black, modifier = Modifier.size(250.dp)) // Reduced size to 250dp
+            }
+            Text(text = "Tablero", fontSize = 40.sp) // Reduced font size to 40sp
+        }
+
+        // Juegos column
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .height(450.dp) // Reduced height to 450dp from 500dp
+                    .width(450.dp) // Reduced width to 450dp from 500dp
+                    .clip(shape = RoundedCornerShape(20.dp))
+                    .background(color = Color(android.graphics.Color.parseColor("#F1B4BB")))
+                    .clickable(onClick = {
+                        navController.navigate("JuegosMenuScreen")
+                    }),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(Icons.Default.Home, contentDescription = "Home", tint = Color.Black, modifier = Modifier.size(250.dp)) // Reduced size to 250dp
+            }
+            Text(text = "Juegos", fontSize = 40.sp) // Reduced font size to 40sp
+        }
+    }
+
+
 }
